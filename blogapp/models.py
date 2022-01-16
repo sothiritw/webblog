e9c979c1256acc675a4d58cebc02528f35d0f52f
Watchara Sothirit <sothirit.w@gmail.com>
2022-01-17 00:38:02 +0700

    change models and change index html

--- blogapp/models.py
+++ blogapp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    link = models.TextField(default="")
+    imageBase64 = models.TextField(default="")
 
     def __str__(self):
         return self.title
